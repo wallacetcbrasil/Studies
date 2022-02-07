7513b3a5159648bd41e5ebefcbb4eb23ebf22824
wallacetcbrasil <wallacetcbrasil.e@gmail.com>
2022-02-07 03:59:52 -0300

    Initial commit

---
+++ In-Progress/Javascript/DevAprender/aula.js
@@ -0,0 +1,19 @@
+console.log('Aulinhas fon fon');
+
+// VARIABLES
+
+// Observation: Preferably use "const"
+
+let hoursOfVideo = 435.31; // If change the value use "let"
+const checkpointVideo = 14.21; // if it is a "Const"ant value use "Const"
+
+console.log(hoursOfVideo);
+console.log(checkpointVideo);
+
+// Type of Variables
+
+let name = "Wallace"; // string is text literally 
+let age = "19"; // Number is Number obviously
+let youAreReading = true; // Boolean is "True" or "False"
+let answer = undefined; // Undefined is "not defined"
+let selectedColor = null; // Null "reset" one value
---
+++ README.md
@@ -0,0 +1,2 @@
+# Studies
+ This is my study folder, it contains projects in progress or unfinished so don't expect "professional projects"

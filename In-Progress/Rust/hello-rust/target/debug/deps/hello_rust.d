47927d62e0ba1bd4da2faecc70e1a3eaae0753ea
wallacetcbrasil <wallacetcbrasil.e@gmail.com>
2022-04-18 16:21:21 -0300

    Start in rust

---
+++ In-Progress/Rust/hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+A:\Trabalho\Programacao\GitHub\Studies\In-Progress\Rust\hello-rust\target\debug\deps\hello_rust.exe: src\main.rs
+
+A:\Trabalho\Programacao\GitHub\Studies\In-Progress\Rust\hello-rust\target\debug\deps\hello_rust.d: src\main.rs
+
+src\main.rs:
---
+++ In-Progress/Rust/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+A:\Trabalho\Programacao\GitHub\Studies\In-Progress\Rust\hello-rust\target\debug\hello-rust.exe: A:\Trabalho\Programacao\GitHub\Studies\In-Progress\Rust\hello-rust\src\main.rs
